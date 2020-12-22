4e787356bda92993db1ebcb72bbe4afc93301ae1
Benny Pritchard <benjamintp.1985@gmail.com>
2020-12-22 14:49:24 +1100

    Green state for all numbers

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,3 +1,9 @@
 def process(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return 'FizzBuzz'
     if number % 3 == 0:
         return 'Fizz'
+    if number % 5 == 0:
+        return 'Buzz'
+    else:
+        return number
